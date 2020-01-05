f0164504cffdeadc5b3582876e20794496514cb1
kawamorikaito <rf.mori64@gmail.com>
2020-01-05 23:04:19 +0900

    games models modification

--- games/models.py
+++ games/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 class Game(models.Model):
-    code = models.CharField(max_length=50)
+    owner_user_code = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
+    game_name = models.CharField(max_length=50, null=True)
     num_people = models.IntegerField(default=0)
-    start_time = models.DateTimeField(default=timezone.now)
+    reception_start_time = models.DateTimeField(default=timezone.now)
+    reception_end_time = models.DateTimeField(default=timezone.now)
+    game_start_time = models.DateTimeField(default=timezone.now)
+    game_end_time = models.DateTimeField(default=timezone.now)
     outline = models.CharField(max_length=50)
     status = models.IntegerField(default=0)
 
--- games/views.py
+++ games/views.py
@@ -1,12 +1,10 @@
+from django.http import Http404
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
-from django.http import Http404
-
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-
 from .models import Game
+
 
 # Create your views here.
 def index(request):
@@ -17,10 +15,12 @@ def index(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def create(request):
     template = loader.get_template('games/create.html')
 
     return render(request, 'games/create.html')
+
 
 def detail(request, game_id):
     try:
@@ -29,12 +29,19 @@ def detail(request, game_id):
         raise Http404("Game does not exist")
     return render(request, 'games/detail.html', {'game': game})
 
+
 def result(request):
     game = Game(
-        code=request.POST['code'],
         name=request.POST['name'],
         num_people=request.POST['num_people'],
-        outline=request.POST['outline']
+        outline=request.POST['outline'],
+        reception_start_time=request.POST['reception_start_time'],
+        reception_end_time=request.POST['reception_end_time'],
+        game_start_time=request.POST['game_start_time'],
+        game_end_time=request.POST['game_end_time'],
+        game_name=request.POST['game_name'],
+        owner_user_code="hogehoge"
+
     )
 
     game.save()
